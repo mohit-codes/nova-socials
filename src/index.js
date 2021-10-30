2d23396a06ae8bbf4729d7ffbd399fb7b34a7873
mohit kushwaha <kushwahamohit000@gmail.com>
2021-10-30 18:49:54 +0530

    feat: Context provider wrapper

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { AuthProvider } from "./Context/authProvider";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
