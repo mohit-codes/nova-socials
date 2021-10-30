da936ab22e989be5f55d384cecf095d65e70aa9d
mohit kushwaha <kushwahamohit000@gmail.com>
2021-10-30 18:48:50 +0530

    feat: reach router added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Router } from "@reach/router";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App h-screen">
+      <Router>
+        <Login path="/" />
+        <Signup path="signup" />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
