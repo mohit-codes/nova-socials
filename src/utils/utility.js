4412d3f681799a4fdf2a221d1e24d9df8413826f
mohit kushwaha <kushwahamohit000@gmail.com>
2021-11-04 18:27:15 +0530

    chore: renamed file to utility

---
+++ src/utils/utility.js
@@ -0,0 +1,7 @@
+export const BASE_URL = "http://localhost:8080";
+
+export function validateEmail(email) {
+  return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+    email
+  );
+}
