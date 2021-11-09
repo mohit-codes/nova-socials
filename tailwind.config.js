dab9889fb75667dcfb553cb294381f089a8ff4fc
mohit kushwaha <kushwahamohit000@gmail.com>
2021-11-09 18:47:59 +0530

    style: theme extend width custom value

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        600: "600px",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
