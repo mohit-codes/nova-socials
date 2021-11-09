a8e2911b28b00df9904f1c847c1ba7d70da97419
mohit kushwaha <kushwahamohit000@gmail.com>
2021-11-09 18:48:58 +0530

    chore: react router v6 implement

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import { Router } from "@reach/router";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import PrivateRoute from "./Components/PrivateRoute";
+import { setUserFromLocalStorage } from "./features/user/userSlice";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setUserFromLocalStorage());
+  }, []);
+
   return (
     <div className="App h-screen">
-      <Router>
-        <Login path="/" />
-        <Signup path="signup" />
-        <PrivateRoute component={Home} path="home" />
-      </Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<PrivateRoute />}>
+          <Route path="/home" element={<Home />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
