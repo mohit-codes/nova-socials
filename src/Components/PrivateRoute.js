bb5987eec7b513147314ed7e4bfd7f37fccc6865
mohit kushwaha <kushwahamohit000@gmail.com>
2021-11-09 18:51:01 +0530

    chore: re-implemented private route with react rouoter v6

--- src/Components/PrivateRoute.js
+++ src/Components/PrivateRoute.js
@@ -1,13 +1,20 @@
-import { Redirect } from "@reach/router";
 import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+import LoadingPage from "./LoadingPage";
 
-const PrivateRoute = ({ component: Component, path }) => {
-  const isUserLoggedIn = useSelector((state) => state.user.isUserLoggedIn);
-  return !isUserLoggedIn ? (
-    <Redirect from="" to="/" noThrow />
-  ) : (
-    <Component path={path} />
-  );
+const PrivateRoute = () => {
+  const userState = useSelector((state) => state.user);
+  const initialLoading = useSelector((state) => state.user.initialLoading);
+
+  if (initialLoading) {
+    return <LoadingPage />;
+  }
+
+  if (userState.isUserLoggedIn) {
+    return <Outlet />;
+  }
+
+  return <Navigate to="/" replace />;
 };
 
 export default PrivateRoute;
