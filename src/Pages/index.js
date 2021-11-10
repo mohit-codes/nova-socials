78186af9f9b7d2ff36a59a3b4e14bb42ddefef69
mohit kushwaha <kushwahamohit000@gmail.com>
2021-11-10 18:43:29 +0530

    feat: exports

---
+++ src/Pages/index.js
@@ -0,0 +1,6 @@
+export { Home } from "./Home";
+export { Login } from "./Login";
+export { Signup } from "./Signup";
+export { Notification } from "./Notification";
+export { Profile } from "./Profile";
+export { Messages } from "./Messages";
