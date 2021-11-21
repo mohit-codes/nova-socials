0829272d8fb6cf739dacaa90342cefe967257b39
mohit kushwaha <kushwahamohit000@gmail.com>
2021-11-21 18:27:42 +0530

    refactor: components export index file

---
+++ src/Components/PostInfoPageComponents/index.js
@@ -0,0 +1,5 @@
+export { Comment } from "./Comment";
+export { Comments } from "./Comments";
+export { EditPost } from "./EditPost";
+export { LikesModal } from "./LikesModal";
+export { NewComment } from "./NewComment";
