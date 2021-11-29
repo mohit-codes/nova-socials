5afeaee68f0e5fc1697d0b4bef3e5f95d38fad03
mohit kushwaha <kushwahamohit000@gmail.com>
2021-11-29 17:02:29 +0530

    style: img tag

--- src/Components/HomePageComponents/Post.js
+++ src/Components/HomePageComponents/Post.js
@@ -9,6 +9,7 @@ export const Post = ({ post }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const userId = useSelector((state) => state.user.data._id);
+  const createdAt = dayjs(post.createdAt).fromNow();
 
   const likeHandler = (e, isLikedByUser) => {
     e.stopPropagation();
@@ -22,16 +23,17 @@ export const Post = ({ post }) => {
       className="p-2 w-full flex border cursor-pointer hover:bg-gray-50"
       onClick={() => navigate(`/post/${post._id}`)}
     >
-      <div
-        className="w-10 h-10 rounded-full bg-gray-200"
+      <img
+        src={post.authorProfileUrl}
+        alt={post.authorName}
+        loading="lazy"
+        className="w-10 h-10 mr-2 rounded-full"
         onClick={(e) => e.stopPropagation()}
-      ></div>
+      />
       <div className="ml-2 w-full">
         <div className="leading-tight" onClick={(e) => e.stopPropagation()}>
           <span className="font-semibold">{post.authorName}</span>
-          <span className="text-gray-400 text-sm float-right">{`${dayjs(
-            post.createdAt
-          ).fromNow()}`}</span>
+          <span className="text-gray-400 text-sm float-right">{createdAt}</span>
         </div>
         <span className="text-gray-400">{`@${post.authorUsername}`}</span>
         <div className="py-5">{post.content}</div>
