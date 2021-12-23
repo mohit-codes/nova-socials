787d00ee3a020acb912080fe01883c7a76956a6f
mohit kushwaha <kushwahamohit000@gmail.com>
2021-12-23 18:57:10 +0530

    refactor: adds message reducer to store

--- src/Store/store.js
+++ src/Store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
 import postReducer from "../features/post/postSlice";
 import notificationReducer from "../features/notification/notificationSlice";
+import messageReducer from "../features/message/messageSlice";
 
 export default configureStore({
   reducer: {
     user: userReducer,
     post: postReducer,
     notification: notificationReducer,
+    message: messageReducer,
   },
 });
