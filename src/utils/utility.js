b849f62226e23f11c733963737255017c8f723da
mohit kushwaha <kushwahamohit000@gmail.com>
2021-12-25 18:14:00 +0530

    feat: adds utility decrypt message using key and iv

--- src/utils/utility.js
+++ src/utils/utility.js
@@ -1,7 +1,18 @@
 export const BASE_URL = "http://localhost:8080";
+import crypto from "crypto-js";
 
 export function validateEmail(email) {
   return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
     email
   );
 }
+
+export const decryptMessage = (key, message, iv) => {
+  let _key = crypto.enc.Hex.parse(key);
+  const result = crypto.AES.decrypt(message, _key, {
+    iv: crypto.enc.Hex.parse(iv),
+    mode: crypto.mode.CBC,
+    format: crypto.format.Hex,
+  }).toString(crypto.enc.Utf8);
+  return result;
+};
