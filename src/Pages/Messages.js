b3eacf399dcb30091a4c667201eaf4351ae84bae
mohit kushwaha <kushwahamohit000@gmail.com>
2021-12-27 18:46:37 +0530

    feat: adds messages page

---
+++ src/Pages/Messages.js
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import SideNavigationBar from "../Components/SideNavigationBar/SideNavigationBar";
+import { useSocket } from "../SocketContext/socketContext";
+import RecipientList from "../Components/MessagesPageComponents/RecipientList";
+import { RiMailAddLine } from "react-icons/ri";
+import InitialSectionCover from "../Components/MessagesPageComponents/InitialSectionCover";
+import { Outlet, useLocation } from "react-router";
+import NewMessageModal from "../Components/MessagesPageComponents/NewMessageModal";
+import Toast from "../Components/Toast/Toast";
+
+const Messages = () => {
+  const user = useSelector((state) => state.user.data);
+  const [showNewMessageModal, setShowNewMessageModal] = useState(false);
+  const socket = useSocket();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    socket.emit("connectUser", { name: user.name });
+  }, []);
+
+  return (
+    <div className="flex h-screen bg-white">
+      <SideNavigationBar />
+      <div className="flex ml-0 md:ml-28 lg:ml-0 w-full">
+        <div className="w-full lg:w-80 border">
+          <div className="h-10 bg-white flex items-center p-2 border">
+            <p className="font-semibold" role="heading">
+              Messages
+            </p>
+            <i
+              className="ml-auto text-xl"
+              onClick={() => setShowNewMessageModal(true)}
+            >
+              <RiMailAddLine />
+            </i>
+          </div>
+          <div className="w-full">
+            <RecipientList />
+          </div>
+        </div>
+        {pathname === "/messages" && (
+          <InitialSectionCover
+            setShowNewMessageModal={setShowNewMessageModal}
+          />
+        )}
+        <Outlet />
+      </div>
+      {showNewMessageModal && (
+        <NewMessageModal setShowNewMessageModal={setShowNewMessageModal} />
+      )}
+      <Toast />
+    </div>
+  );
+};
+
+export default Messages;
